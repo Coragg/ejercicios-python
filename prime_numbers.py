8897fb5a81e2a055f14304a71b71e4ef7c589034
Coragg <v_e_c_g@outlook.com>
2021-07-13 15:16:20 -0400

    correct line 14

--- prime_numbers.py
+++ prime_numbers.py
@@ -11,7 +11,7 @@ print(defind_of_number)
 # search in a range everything the prime numbers
 range_of_number = 200 # change this number for search a primes numbers
 list_of_prime_numbers = []
-for number in range(1, range_of_number+1):
+for number in range(2, range_of_number+1):
   list_of_prime_numbers.append(number)
   for each_number in range(2, number):
     if number % each_number == 0:
